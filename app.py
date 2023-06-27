3db4d8353f036571e878917e074567686a30c2d3
majikojima <ykk61890@yahoo.co.jp>
2023-06-27 18:08:25 +0900

    Add feature to summarize input text and output to text file

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
 import openai
 
+input_file = input("要約したい対象のファイルのパス： ")
+with open(input_file, encoding='utf-8') as f:
+    text = f.read()
+
 # [https://platform.openai.com/docs/guides/gpt/chat-completions-api]
 res = openai.ChatCompletion.create(
     model="gpt-3.5-turbo",
     messages=[
         {"role": "system", "content": "You are a helpful assistant.yeah!"},
-        {"role": "user", "content": "unixの考え方を教えて？"},
-        {"role": "assistant", "content": "Unixの設計哲学は、以下のようなものです。1. 単一の目的を持ったプログラムを作る。2. プログラム同士は、テキストベースでやり取りする。3. 新しい機能を実装する前に、既存のシステムを最大限に活用しようとする。4. プログラムは、権限を最小限に抑えて実行される。5. 出力は、可能な限りシンプルなテキスト形式で行われる。これらの考え方は、シンプルで柔軟性が高く、理解しやすいシステムを構築するために重要です。また、これらの考え方は、Unixが長期にわたって広く使われてきた一因でもあります。"},
-        {"role": "user", "content": "要約してください"},
+        {"role": "user", "content": f"この文章を要約して。「{text}」"},
     ]
 )
 
 res_content = res["choices"][0]["message"]["content"]
-print(res_content)
+
+with open("output.txt", "w") as f:
+    f.write(res_content)
