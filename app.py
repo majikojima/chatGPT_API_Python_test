8ebf4ec2ab4725dd9498d74934c751b1139c5ab7
majikojima <ykk61890@yahoo.co.jp>
2023-06-27 17:37:36 +0900

    wrap up

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ res = openai.ChatCompletion.create(
     messages=[
         {"role": "system", "content": "You are a helpful assistant.yeah!"},
         {"role": "user", "content": "unixの考え方を教えて？"},
+        {"role": "assistant", "content": "Unixの設計哲学は、以下のようなものです。1. 単一の目的を持ったプログラムを作る。2. プログラム同士は、テキストベースでやり取りする。3. 新しい機能を実装する前に、既存のシステムを最大限に活用しようとする。4. プログラムは、権限を最小限に抑えて実行される。5. 出力は、可能な限りシンプルなテキスト形式で行われる。これらの考え方は、シンプルで柔軟性が高く、理解しやすいシステムを構築するために重要です。また、これらの考え方は、Unixが長期にわたって広く使われてきた一因でもあります。"},
+        {"role": "user", "content": "要約してください"},
     ]
 )
 
